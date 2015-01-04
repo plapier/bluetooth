4c5cb5bbace2464b526c40c66f364d4d695f7dcf
Phil LaPier <phil.lapier@gmail.com>
2015-01-04 11:08:38 -0800

    Initial Commit

---
+++ Bluetooth/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Bluetooth
+//
+//  Created by Phil LaPier on 1/4/15.
+//  Copyright (c) 2015 Phil LaPier. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
